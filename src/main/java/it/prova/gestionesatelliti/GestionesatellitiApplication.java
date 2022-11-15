35e1e84e5ca450b40e45a134c230433e660e47f7
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-15 16:17:32 +0100

    first commit

---
+++ src/main/java/it/prova/gestionesatelliti/GestionesatellitiApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionesatelliti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionesatellitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionesatellitiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionesatelliti/GestionesatellitiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionesatelliti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionesatellitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
